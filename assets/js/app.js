838bc805ee507167991efcdffb63e075546ea99c
Bryan McBride <mcbride.bryan@gmail.com>
2021-10-01 16:43:24 -0400

    initial code commit

---
+++ assets/js/app.js
@@ -0,0 +1,130 @@
+const map = L.map("map", {
+  zoomSnap: L.Browser.mobile ? 0 : 1,
+  tap: false,
+  maxZoom: 22,
+  zoomControl: false,
+  renderer: L.canvas({
+    padding: 0.5,
+    tolerance: 10
+  })
+}).fitWorld();
+map.attributionControl.setPrefix("");
+
+const layers = {
+  basemaps: {
+    "Streets": L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.@2xpng", {
+      maxNativeZoom: 18,
+      maxZoom: map.getMaxZoom(),
+      attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attribution">CARTO</a>',
+    }),
+    "Aerial": L.tileLayer("https://orthos.its.ny.gov/arcgis/rest/services/wms/2017/MapServer/tile/{z}/{y}/{x}", {
+      maxNativeZoom: 18,
+      maxZoom: map.getMaxZoom(),
+      attribution: "NYS ITS - GPO",
+    }),
+    "Topo": L.tileLayer("https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}", {
+      maxNativeZoom: 16,
+      maxZoom: map.getMaxZoom(),
+      attribution: "USGS",
+    }),
+    "Trail Map": L.tileLayer.mbTiles("data/trail_map.mbtiles", {
+      autoScale: true,
+      fitBounds: true,
+      updateWhenIdle: false
+    }).on("databaseloaded", (e) => {
+      map.setMaxBounds(L.latLngBounds(layers.basemaps["Trail Map"].options.bounds).pad(0.1));
+      controls.locateCtrl.start();
+    })
+  }
+};
+
+/*** Begin Zoom Extent Control ***/
+L.Control.ZoomExtent = L.Control.extend({
+  onAdd: function(map) {    
+    const div = L.DomUtil.create("div", "leaflet-bar leaflet-control");
+    div.innerHTML = `
+      <a class='leaflet-bar-part leaflet-bar-part-single zoom-extent-btn' title='Zoom To Map' onclick='ZoomToExtent();'>
+        <i class='icon-zoom_out_map'></i>
+      </a>
+    `;
+    L.DomEvent.on(div, "click", function (e) {
+      L.DomEvent.stopPropagation(e);
+    });
+    return div
+  }
+});
+
+L.control.zoomextent = (opts) => {
+  return new L.Control.ZoomExtent(opts);
+}
+/*** End custom control ***/
+
+const controls = {
+  layerCtrl: L.control.layers(layers.basemaps, null, {
+    collapsed: true,
+    position: "topright"
+  }).addTo(map),
+
+  zoomCtrl: L.control.zoomextent({
+    position: "bottomright"
+  }).addTo(map),
+
+  locateCtrl: L.control.locate({
+    icon: "icon-gps_fixed",
+    iconLoading: "spinner icon-gps_fixed",
+    setView: "untilPan",
+    cacheLocation: true,
+    position: "bottomright",
+    flyTo: false,
+    keepCurrentZoomLevel: true,
+    circleStyle: {
+      interactive: false
+    },
+    markerStyle: {
+      interactive: true
+    },
+    metric: false,
+    strings: {
+      title: "My location",
+      outsideMapBoundsMsg: "You seem to be located outside the preserve boundary!",
+      popup: (options) => {
+        const loc = controls.locateCtrl._marker.getLatLng();
+        return `<div style="text-align: center;">You are within ${Number(options.distance).toLocaleString()} ${options.unit}<br>from <strong>${loc.lat.toFixed(6)}</strong>, <strong>${loc.lng.toFixed(6)}</strong></div>`;
+      }
+    },
+    locateOptions: {
+      enableHighAccuracy: true,
+      maxZoom: 18
+    },
+    onLocationError: (e) => {
+      hideLoader();
+      document.querySelector(".leaflet-control-locate").getElementsByTagName("span")[0].className = "icon-gps_off";
+      alert(e.message);
+    }
+  }).addTo(map),
+
+  scaleCtrl: L.control.scale({
+    position: "bottomleft"
+  }).addTo(map)
+};
+
+function ZoomToExtent() {
+  map.fitBounds(layers.basemaps["Trail Map"].options.bounds);
+}
+
+function showLoader() {
+  document.getElementById("progress-bar").style.display = "block";
+}
+
+function hideLoader() {
+  document.getElementById("progress-bar").style.display = "none";
+}
+
+initSqlJs({
+  locateFile: function() {
+    return "assets/vendor/sqljs-1.6.2/sql-wasm.wasm";
+  }
+}).then(function(SQL){
+  hideLoader();
+  layers.basemaps["Trail Map"].addTo(map);
+});
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+importScripts('assets/vendor/workbox-6.1.5/workbox-sw.js');
+
+workbox.setConfig({
+  debug: false,
+  modulePathPrefix: 'assets/vendor/workbox-6.1.5/'
+});
+
+workbox.precaching.precacheAndRoute([
+  {url: 'index.html', revision: '10.01.21.1'},
+  {url: 'manifest.json', revision: '10.01.21.1'},
+  {url: 'assets/js/app.js', revision: '10.01.21.1'},
+  {url: 'assets/css/app.css', revision: '10.01.21.1'},
+  {url: 'assets/img/apple-touch-icon.png', revision: '10.01.21.1'},
+  {url: 'assets/img/favicon-32x32.png', revision: '10.01.21.1'},
+  {url: 'assets/img/favicon-16x16.png', revision: '10.01.21.1'},
+  {url: 'assets/vendor/icomoon/style.css', revision: '10.01.21.1'},
+  {url: 'assets/vendor/icomoon/fonts/icomoon.ttf', revision: '10.01.21.1'},
+  {url: 'assets/vendor/icomoon/fonts/icomoon.woff', revision: '10.01.21.1'},
+  {url: 'assets/vendor/sqljs-1.6.2/sql-wasm.js', revision: '10.01.21.1'},
+  {url: 'assets/vendor/sqljs-1.6.2/sql-wasm.wasm', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/images/layers.png', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/images/layers-2x.png', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/images/marker-icon.png', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/images/marker-icon-2x.png', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/images/marker-shadow.png', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/leaflet.css', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-1.7.1/leaflet.js', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-locatecontrol-0.74.0/L.Control.Locate.min.css', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-locatecontrol-0.74.0/L.Control.Locate.min.js', revision: '10.01.21.1'},
+  {url: 'assets/vendor/leaflet-mbtiles/Leaflet.TileLayer.MBTiles.js', revision: '10.01.21.1'}
+], {
+  // Ignore all URL parameters.
+  ignoreURLParametersMatching: [/.*/]
+});
